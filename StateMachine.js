fa5c19fae75933a93f3c12f08977a5755890adee
Oliver Caldwell <olliec87@gmail.com>
2012-09-16 15:51:09 +0100

    Began the construction of the class. Taking into account the existence of AMD.

--- StateMachine.js
+++ StateMachine.js
@@ -3,3 +3,33 @@
  * Oliver Caldwell
  * MIT license
  */
+
+;(function(exports) {
+    /**
+     * Builds the class and returns it to be exposed as you wish
+     *
+     * @param {Function} EventEmitter Event class required by this class for extension, you decide how to load it, AMD/script tag etc
+     * @return {Function} This will be the build StateMachine class to be exposed either via AMD or the global object
+     */
+    function construct(EventEmitter) {
+        function StateMachine(){}
+        return StateMachine;
+    }
+
+    // We need to build the StateMachine class using the construct function
+    // This construction will occur differently depending on the existence of AMD
+    if(typeof define === 'function' && define.amd) {
+        // In this case, AMD is present on the page
+        // We will assume (if wrong, I'm sorry) that the script is being loaded with it
+        // So we should load EventEmitter from the components directory via AMD too
+        define(['./components/eventEmitter/EventEmitter.js'], construct);
+    }
+    else {
+        // Without AMD on the page they must load EventEmitter manually
+        // Which also means StateMachine must be built and exposed manually
+        // So we use the global EventEmitter instance and drop StateMachine into the global object too
+        exports.StateMachine = construct(EventEmitter);
+
+        // If the dependency is not met the user will probably get an undefined warning
+    }
+}(this));
